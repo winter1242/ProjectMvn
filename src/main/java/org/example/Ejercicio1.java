9c0221882a1e81ebb528399502a4a006570d8696
winter1242 <jeanpierr-28@hotmail.com>
2024-09-27 18:39:23 -0500

    Correccion ejercicio 01

--- src/main/java/org/example/Ejercicio1.java
+++ src/main/java/org/example/Ejercicio1.java
@@ -1,18 +1,34 @@
 package org.example;
 
-public class ClassA {
-    public static void main() {
+import java.util.Random;
 
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        int[] miArray = generator();
+
+        multiplicarPorDos(miArray);
+
+
+        for (int valor : miArray) {
+            System.out.print(valor + " ");
+        }
     }
 
-    public int[] multpx(int[] array) {
+    public static int[] generator() {
         int[] miArray = new int[500];
+        Random random = new Random();
 
-
-        for (int i = 0; i < 500; i++) {
-            int multi = array[i] * 2;
-            miArray[i] = multi;
+        for (int i = 0; i < miArray.length; i++) {
+            miArray[i] = random.nextInt(1000) + 1;
         }
+        
         return miArray;
+    }
+
+    public static void multiplicarPorDos(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= 2;
+        }
     }
 }
