2e0d440c5ffad456d5016015fd6164b352abcb69
winter1242 <jeanpierr-28@hotmail.com>
2024-10-01 21:41:37 -0500

    Se agrego el ejercicio 01

---
+++ src/main/java/principal/Ejercicio1.java
@@ -0,0 +1,32 @@
+package principal;
+
+import modelos.Usuario;
+import reader.ExcelReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio1 {
+    public static void main(String args[]) {
+        final var listatotal = ExcelReader.obtenerListaUsuarios();
+        //System.out.println(listatotal);
+        final var listafiltrada = filtrarUsuarios(listatotal);
+
+        System.out.printf("La lista de Usuarios es :%s", listafiltrada);
+
+    }
+
+    private static List<Usuario> filtrarUsuarios(List<Usuario> listaUsuarios) {
+        final var listFiltrada = new ArrayList<Usuario>();
+
+        for (var usuario : listaUsuarios) {
+            if (usuario.getPeso() > 50 && usuario.getNombre().startsWith("R")) {
+                listFiltrada.add(usuario);
+            }
+        }
+        return listFiltrada;
+
+    }
+
+
+}
