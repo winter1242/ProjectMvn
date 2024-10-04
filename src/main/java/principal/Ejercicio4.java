7f6664622346462e33d7be857ab1287c5e56b7ab
winter1242 <jeanpierr-28@hotmail.com>
2024-10-03 20:55:03 -0500

    Se agrego el ejercicio 4

---
+++ src/main/java/principal/Ejercicio4.java
@@ -0,0 +1,31 @@
+package principal;
+
+import modelos.Usuario;
+import reader.ExcelReader;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Ejercicio4 {
+    public static void main(String args[]) {
+        final var listUsuario = ExcelReader.obtenerListaUsuarios();
+        final var listOrdenada = ordenarUsuario(listUsuario);
+
+        System.out.printf("La lista de usuarios ordenada es : %s", listOrdenada);
+
+
+    }
+
+    private static List<Usuario> ordenarUsuario(List<Usuario> listUsuario) {
+
+        Collections.sort(listUsuario, new Comparator<Usuario>() {
+            @Override
+            public int compare(Usuario o1, Usuario o2) {
+                return o2.getApellido().compareTo(o1.getApellido());
+            }
+        });
+
+        return listUsuario;
+    }
+}
