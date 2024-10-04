f0cf809055c6e07066aa6fdf43f9e887b43ded78
winter1242 <jeanpierr-28@hotmail.com>
2024-10-03 21:15:45 -0500

    Se agrego el ejercicio3

--- src/main/java/principal/Ejercicio3.java
+++ src/main/java/principal/Ejercicio3.java
@@ -1,2 +1,28 @@
-package principal;public class Ejercicio3 {
+package principal;
+
+import modelos.Usuario;
+import reader.ExcelReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio3 {
+    public static void main(String args[]) {
+        final var listUsuario = ExcelReader.obtenerListaUsuarios();
+        final var listFiltrada = listChina(listUsuario);
+
+        System.out.printf("La lista de Usuario es : %s", listFiltrada);
+
+    }
+
+    public static List<Usuario> listChina(List<Usuario> listUsuario) {
+        final var listFiltrada = new ArrayList<Usuario>();
+
+        for (var usuario : listUsuario) {
+            if (usuario.getPais() == "CHINA") {
+                listFiltrada.add(usuario);
+            }
+        }
+        return listFiltrada;
+    }
 }
