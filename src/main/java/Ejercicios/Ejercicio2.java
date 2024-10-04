459c672be1c9091c238fe0e3e6ba0208c8de4f8f
winter1242 <jeanpierr-28@hotmail.com>
2024-10-04 00:49:58 -0500

    Se agrego el ejercicio 2

---
+++ src/main/java/Ejercicios/Ejercicio2.java
@@ -0,0 +1,33 @@
+package Ejercicios;
+
+import modelos.Pokemon;
+import reader.JsonReader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Ejercicio2 {
+    public static void main(String args[]) {
+        final var listPokemon = JsonReader.leerPokemonJson();
+        final var listFiltrada = filtrarPokemon(listPokemon);
+        System.out.printf("La lista es : %s", listFiltrada);
+    }
+
+    private static List<Pokemon> filtrarPokemon(List<Pokemon> listPokemon) {
+        final var listFiltrada = new ArrayList<Pokemon>();
+        for (var pokemon : listPokemon) {
+            if (pokemon.getTipo().equals("ELECTRICIDAD")) {
+                listFiltrada.add(pokemon);
+            }
+        }
+        Collections.sort(listFiltrada, new Comparator<Pokemon>() {
+            @Override
+            public int compare(Pokemon o1, Pokemon o2) {
+                return Double.compare(o2.getNivel(), o1.getNivel());
+            }
+        });
+        return listFiltrada;
+    }
+}
