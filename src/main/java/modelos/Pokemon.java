5045c067485ec6f97beb761c0738bcbc06ab51e0
winter1242 <jeanpierr-28@hotmail.com>
2024-10-04 00:42:07 -0500

    Se agrego el ejercicio 01

---
+++ src/main/java/modelos/Pokemon.java
@@ -0,0 +1,87 @@
+package modelos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Pokemon {
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("nombre")
+    private String nombre;
+    @JsonProperty("nivel")
+    private int nivel;
+    @JsonProperty("hp")
+    private int hp;
+    @JsonProperty("ataque")
+    private double ataque;
+    @JsonProperty("defensa")
+    private double defensa;
+    @JsonProperty("ataqueEspecial")
+    private double ataqueEspecial;
+    @JsonProperty("defensaEspecial")
+    private double defensaEspecial;
+    @JsonProperty("velocidad")
+    private double velocidad;
+    @JsonProperty("esMacho")
+    private Boolean esMacho;
+    @JsonProperty("entrenador")
+    private String entrenador;
+    @JsonProperty("ubicacion")
+    private String ubicacion;
+    @JsonProperty("tipo")
+    private String tipo;
+
+    public Boolean getEsMacho() {
+        return esMacho;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getDefensa() {
+        return defensa;
+    }
+
+    @Override
+    public String toString() {
+        final var multilinea = """
+                {
+                    id : %s,
+                    nombre : %s,
+                    nivel : %d,
+                    hp : %d,
+                    ataque : %.2f,
+                    defensa : %.2f,
+                    ataqueEspecial : %.2f,
+                    defensaEspecial : %.2f,
+                    velocidad : %.2f,
+                    esMacho : %b,
+                    entrenador : %s,
+                    ubicacion : %s,
+                    tipo : %s
+                }
+                """;
+        return String.format(multilinea, id, nombre, nivel, hp, ataque, defensa, ataqueEspecial, defensaEspecial, velocidad, esMacho, entrenador, ubicacion, tipo);
+
+    }
+}
---
+++ src/main/java/reader/JsonReader.java
@@ -0,0 +1,27 @@
+package reader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import modelos.Pokemon;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonReader {
+    private final static String pokemonPath = "src/main/resources/json/pokemones.json";
+
+    public static List<Pokemon> leerPokemonJson() {
+        final var objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(
+                    new File(pokemonPath),
+                    TypeFactory.defaultInstance()
+                            .constructCollectionType(List.class, Pokemon.class)
+            );
+        } catch (IOException ioException) {
+            System.out.println("Error : " + ioException.getLocalizedMessage());
+            throw new RuntimeException();
+        }
+    }
+}
