4c6535a339a4a64f2c5af072e68342a2f4f4d8f0
winter1242 <jeanpierr-28@hotmail.com>
2024-10-18 20:40:32 -0500

    Correccion Random Utilities

--- src/main/java/dataInt/RandomUtilities.java
+++ src/main/java/dataInt/RandomUtilities.java
@@ -6,7 +6,7 @@ public class RandomUtilities {
     private static final Faker faker = new Faker();
 
     public static int[] generator(int min, int max, int cant) {
-        int[] miArray = new int[cant];
+        final var miArray = new int[cant];
 
         for (int i = 0; i < miArray.length; i++) {
             miArray[i] = faker.number().numberBetween(min, max);
